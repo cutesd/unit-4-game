22d952d2f92d23b590e66762f544aafc266bfc29
Kerri Coupar <kimberly@kimberlycooper.me>
2018-06-14 13:51:55 -0700

    Added Play Again button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -44,6 +44,7 @@ $(function () {
         var comment = $('<div>');
 
         var attack_btn = $('<btn>');
+        var playAgain_btn = $('<btn>');
         var restart_btn = $('<btn>');
 
 
@@ -60,7 +61,11 @@ $(function () {
             //
             attack_btn.addClass('btn btn-lg btn-danger attack-btn');
             attack_btn.text('ATTACK');
-            // attack_btn.on("click")
+            attack_btn.on("click", attack);
+            //
+            playAgain_btn.addClass('btn btn-lg btn-info playAgain-btn');
+            playAgain_btn.text('Play Again');
+            playAgain_btn.on("click",playAgain);
             //
             restart_btn.addClass('btn btn-lg btn-warning restart-btn');
             restart_btn.text('Restart');
@@ -96,26 +101,69 @@ $(function () {
         }
 
         function playerSelect(e) {
-            console.log(this.id);
+            console.log($(this)[0].id);
             if (player == undefined) {
-                player = player_arr[this.id.substr(-1)];
-                playerSlot.append(this);
+                player = player_arr[$(this)[0].id.substr(-1)];
+                playerSlot.append($(this));
                 console.log(player);
             } else if (opponent == undefined) {
-                opponent = player_arr[this.id.substr(-1)];
-                opponentSlot.append(this);
+                opponent = player_arr[$(this)[0].id.substr(-1)];
+                opponentSlot.append($(this));
                 console.log(opponent);
                 beginAttack();
             }
         }
 
-        function beginAttack (){
+        function beginAttack() {
             battleground.append(playerSlot);
             battleground.append(attack_btn);
             battleground.append(opponentSlot);
             battleground.append(comment);
             playerChoose.remove();
             main.append(battleground);
+        }
+
+        function attack(e) {
+            // PLAYER ATTACKS
+            opponent.health -= player.pwr.attack;
+            // OPPONENT COUNTERATTACKS
+            player.health -= opponent.pwr.counter;
+            //
+            healthUpdate(player);
+            healthUpdate(opponent);
+            //
+            if (player.health <= 0) {
+                //YOU LOSE
+                commentaryUpdate('<p>You have been defeated ... GAME OVER!!</p>');
+            } else if (opponent.health <= 0) {
+                //YOU WIN
+                commentaryUpdate('<p>You have defeated '+ opponent.name+ '. Choose another opponent.</p');
+                attack_btn.replaceWith(playAgain_btn);
+            } else {
+                commentaryUpdate();
+                player.pwr.attack += player.pwr.powerBase;
+            }
+        }
+
+        function healthUpdate(player) {
+            var pID = "#p" + player.pNum;
+            $(pID).find('.health').text(player.health);
+        }
+
+        function commentaryUpdate(str) {
+            if (str == undefined) {
+                comment.html('<p>You attacked ' + opponent.name + ' for ' + player.pwr.attack + ' damage.</p><p>' + opponent.name + ' attacked you back for ' + opponent.pwr.counter + ' damage.</p >');
+            }else{
+                comment.html(str);
+            }
+        }
+
+        function playAgain(){
+            opponent = undefined;
+            $.each(battleground, function(i,div){
+                div.remove();
+            });
+
         }
 
     }
